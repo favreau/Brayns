d5f442097dea3a3cdfc66f9e652d8f9593fa5592
Cyrille Favreau <cyrille.favreau@epfl.ch>
2017-05-17 17:24:58 +0200

    Added deflect streaming module

---
+++ plugins/engines/ospray/OSPRayPixelOperations.cpp
@@ -0,0 +1,6 @@
+#include "OSPRayPixelOperations.h"
+
+OSPRayPixelOperations::OSPRayPixelOperations()
+{
+
+}
---
+++ plugins/engines/ospray/OSPRayPixelOperations.h
@@ -0,0 +1,11 @@
+#ifndef OSPRAYPIXELOPERATIONS_H
+#define OSPRAYPIXELOPERATIONS_H
+
+
+class OSPRayPixelOperations
+{
+public:
+    OSPRayPixelOperations();
+};
+
+#endif // OSPRAYPIXELOPERATIONS_H

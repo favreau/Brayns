7594569b7890142186b7a5a3e7d92ba0e671c4c2
Cyrille Favreau <cyrille.favreau@epfl.ch>
2018-11-17 00:54:24 +0100

    Removed impl pattern from circuit loader

--- plugins/CircuitExplorerPlugin/io/CircuitLoader.h
+++ plugins/CircuitExplorerPlugin/io/CircuitLoader.h
@@ -20,11 +20,15 @@
 
 #pragma once
 
+#include "MorphologyLoader.h"
 #include "api/CircuitExplorerParams.h"
 #include "types.h"
 
 #include <brayns/common/loader/Loader.h>
 #include <brayns/common/types.h>
+
+#include <brain/brain.h>
+#include <brion/brion.h>
 
 #include <set>
 #include <vector>
@@ -65,7 +69,63 @@ public:
      */
     brayns::ModelDescriptorPtr importCircuit(const std::string& circuitConfig);
 
+    /**
+     * @brief _populateLayerIds populates the neuron layer IDs. This is
+     * currently only supported for the MVD2 format.
+     * @param blueConfig Configuration of the circuit
+     * @param gids GIDs of the neurons
+     */
+    size_ts _populateLayerIds(const brion::BlueConfig& blueConfig,
+                              const brain::GIDSet& gids) const;
+
 private:
-    class Impl;
-    std::unique_ptr<const Impl> _impl;
+    std::vector<std::string> _getTargetsAsStrings(
+        const std::string& targets) const;
+
+    /**
+     * @brief _logLoadedGIDs Logs selected GIDs for debugging purpose
+     * @param gids to trace
+     */
+    void _logLoadedGIDs(const brain::GIDSet& gids) const;
+
+    std::string _getMeshFilenameFromGID(const uint64_t gid) const;
+
+#if BRAYNS_USE_ASSIMP
+    bool _importMeshes(brayns::Model& model, const brain::GIDSet& gids,
+                       const brayns::Matrix4fs& transformations,
+                       const GIDOffsets& targetGIDOffsets,
+                       const size_ts& layerIds, const size_ts& morphologyTypes,
+                       const size_ts& electrophysiologyTypes) const;
+#endif
+
+    bool _importMorphologies(const brain::Circuit& circuit,
+                             brayns::Model& model, const brain::GIDSet& gids,
+                             const brayns::Matrix4fs& transformations,
+                             const GIDOffsets& targetGIDOffsets,
+                             CompartmentReportPtr compartmentReport,
+                             MorphologyLoader& morphLoader,
+                             const size_ts& layerIds,
+                             const size_ts& morphologyTypes,
+                             const size_ts& electrophysiologyTypes) const;
+
+    /**
+     * @brief _getMaterialFromSectionType return a material determined by the
+     * --color-scheme geometry parameter
+     * @param index Index of the element to which the material will attached
+     * @param material Material that is forced in case geometry parameters
+     * do not apply
+     * @param sectionType Section type of the geometry to which the material
+     * will be applied
+     * @return Material ID determined by the geometry parameters
+     */
+    size_t _getMaterialFromCircuitAttributes(
+        const uint64_t index, const size_t material,
+        const GIDOffsets& targetGIDOffsets, const size_ts& layerIds,
+        const size_ts& morphologyTypes, const size_ts& electrophysiologyTypes,
+        bool isMesh = false) const;
+
+    const brayns::ApplicationParameters& _applicationParameters;
+    brayns::AnimationParameters& _animationParameters;
+    const CircuitAttributes& _circuitAttributes;
+    const MorphologyAttributes& _morphologyAttributes;
 };
